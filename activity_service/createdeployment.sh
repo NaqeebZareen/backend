9d818ef3445cb61128eebf5fdf6579a1d123f9cd
nadeem2 <nadeem.qasmi@ukan.co>
2020-08-23 13:41:45 +0500

    updated 23 aug 1:41

--- activity_service/createdeployment.sh
+++ activity_service/createdeployment.sh
@@ -1,4 +1,4 @@
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t activityservice:1.1.2 .
-docker tag activityservice:1.1.2 034363173241.dkr.ecr.us-east-1.amazonaws.com/activityservice:1.1.2
-docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/activityservice:1.1.2
+docker build -t activityservice:1.1.3 .
+docker tag activityservice:1.1.3 034363173241.dkr.ecr.us-east-1.amazonaws.com/activityservice:1.1.3
+docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/activityservice:1.1.3
--- news_service/createdeployment.sh
+++ news_service/createdeployment.sh
@@ -1,4 +1,4 @@
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t news:1.1.4 .
-docker tag news:1.1.4 034363173241.dkr.ecr.us-east-1.amazonaws.com/news:1.1.4
-docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/news:1.1.4
+docker build -t news:1.1.5 .
+docker tag news:1.1.5 034363173241.dkr.ecr.us-east-1.amazonaws.com/news:1.1.5
+docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/news:1.1.5
