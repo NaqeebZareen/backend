45b41c03da4ceaa01bc969a6fc3675c829f57dcf
nadeem2 <nadeem.qasmi@ukan.co>
2020-08-26 13:58:38 +0500

    Update

--- server/createdeployment.sh
+++ server/createdeployment.sh
@@ -1,4 +1,4 @@
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t gateway:1.0.7 .
-docker tag gateway:1.0.7 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.7
-docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.7
+docker build -t gateway:1.0.8 .
+docker tag gateway:1.0.8 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.8
+docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.8
