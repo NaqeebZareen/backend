ec453b202fbaa879c1bb1dd5ad2b0e09676c1516
= <=>
2020-10-14 19:33:07 +0500

    create new deployment for newsletter service

--- newsletter_service/createdeployment.sh
+++ newsletter_service/createdeployment.sh
@@ -1,4 +1,4 @@
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t news:0.0.2 .
-docker tag news:0.0.2 034363173241.dkr.ecr.us-east-1.amazonaws.com/news:0.0.2
-docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/news:0.0.2
+docker build -t newsletter:0.0.1 .
+docker tag newsletter:0.0.1 034363173241.dkr.ecr.us-east-1.amazonaws.com/newsletter:0.0.1
+docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/newsletter:0.0.1
--- server/createdeployment.sh
+++ server/createdeployment.sh
@@ -1,4 +1,4 @@
 $(aws ecr get-login --no-include-email --region us-east-1)
-docker build -t gateway:1.0.9 .
-docker tag gateway:1.0.9 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.9
-docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.9
+docker build -t gateway:1.0.10 .
+docker tag gateway:1.0.10 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.10
+docker push 034363173241.dkr.ecr.us-east-1.amazonaws.com/gateway:1.0.10
